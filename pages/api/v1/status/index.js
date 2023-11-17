e5f12530e504a67e432de86a8b3b275941492ff2
João Vitor <69482936+Jottinha@users.noreply.github.com>
2023-11-17 17:11:59 +0000

    Add `database.js` e `.env` on project

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import dataBase from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const queryResponse = await dataBase.query("SELECT 1 + 1 AS sum");
+  console.log(queryResponse.rows);
   response.status(200).send("Tudo certo por aqui");
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,5 @@
-test("GET to /api/v1/status should return 200", () => {
-  fetch("http://localhost:3000/api/v1/status").then((response) => {
+test("GET to /api/v1/status should return 200", async () => {
+  await fetch("http://localhost:3000/api/v1/status").then((response) => {
     expect(response.status).toBe(200);
   });
 });
